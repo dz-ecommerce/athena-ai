06e7e8b84f6911a978c6bd4c1045243d93f82bc0
dz-ecommerce <info@dz-ecom.de>
2025-05-30 19:36:47 +0000

    Bump version to 1.0.2 [skip ci]

--- athena-ai.php
+++ athena-ai.php
@@ -20,7 +20,7 @@ if (!defined('WPINC')) {
 }
 
 // Define plugin constants
-define('ATHENA_AI_VERSION', '1.0.1');
+define('ATHENA_AI_VERSION', '1.0.2');
 define('ATHENA_AI_PLUGIN_FILE', __FILE__);
 define('ATHENA_AI_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ATHENA_AI_PLUGIN_URL', plugin_dir_url(__FILE__));
